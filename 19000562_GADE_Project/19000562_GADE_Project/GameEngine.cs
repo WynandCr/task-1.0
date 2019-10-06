e970e8d6238092bab3abca1fef657fd8908493fc
Wynand <wynandcronje99@gmail.com>
2019-10-06 17:23:18 +0200

    Task 1 of POE

---
+++ 19000562_GADE_Project/19000562_GADE_Project/GameEngine.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace _19000562_GADE_Project
+{
+    class GameEngine
+    {
+        public Map myMap = new Map();//new map object
+        private Form1 form;//making form
+        private GroupBox messageGroup;//new groupbox object
+
+        public GameEngine(Form1 form, GroupBox messageGroup)
+        {
+            this.form = form;//making the new form equal to the form1
+            this.messageGroup = messageGroup;
+            foreach (Unit u in myMap.units)
+            {
+                Button b = new Button();//new button with size 30 by 30
+                b.Location = new Point(u.X * 35, u.Y * 35);
+                b.Size = new Size(30, 30);
+                b.Text = u.Symbol;
+                
+                if(u.Team == 0)//if team equal 0 the background is red otherwise background is green
+                {
+                    b.BackColor = Color.Red;
+                }
+                else
+                {
+                    b.BackColor = Color.Green;
+                }
+
+                if(u.GetType() == typeof(MeleeUnit))//testing is its range or melee unit
+                {
+                    b.ForeColor = Color.White;//text on button can be black or white depending on which unit it is
+                }
+                else
+                {
+                    b.ForeColor = Color.Black;
+                }
+
+                b.Click += buttonClick;//if button is clicked it updates button method info
+                this.form.Controls.Add(b);
+            }
+        }
+
+        public void UpdateDisplay()
+        {
+            
+            form.Controls.Clear();
+            form.Controls.Add(messageGroup);
+            foreach (Unit u in myMap.units)
+            {
+                Button b = new Button();//new button with size of button
+                b.Location = new Point(u.X * 35, u.Y * 35);
+                b.Size = new Size(30, 30);
+                b.Text = u.Symbol;
+
+                if(u.Team == 0)
+                {
+                    b.BackColor = Color.Red;//if team equal 0 the background is red otherwise background is green
+                }
+                else
+                {
+                    b.BackColor = Color.Green;
+                }
+
+                if(u.GetType() == typeof(MeleeUnit))//testing is its range or melee unit
+                {
+                    b.ForeColor = Color.White;//if button is clicked it updates button method info
+                }
+                else
+                {
+                    b.ForeColor = Color.Black;
+                }
+
+                b.Click += buttonClick;//if button is clicked it updates button method info
+                form.Controls.Add(b);
+            }                     
+        }
+
+        public void UpdateMap()
+        {
+            //updating the map after each round
+            foreach (Unit u in myMap.units)
+            {
+                Unit closestUnit = u.Closest(ref myMap.units);//finding the closest unit
+                try
+                {
+                    u.Move(ref closestUnit);
+                }
+                catch(MeleeUnit.DeathException d)//if it doesnt move its death
+                {
+                    form.displayInfo(d.Message);
+                    Unit[] temp = new Unit[myMap.units.Count() - 1];
+                    int j = 0;
+                    for(int i = 0; i < myMap.units.Count(); i++)
+                    {
+                        if(u != myMap.units[i])
+                        {
+                            temp[j++] = myMap.units[i];
+                        }
+                    }
+                    myMap.units = temp;
+                }
+            }           
+        }
+
+        public void buttonClick(object sender, EventArgs args)
+        {
+            foreach (Unit u in myMap.units)
+            {
+                if(((Button)sender).Text == u.Symbol)//if btn is click then display info in textlist
+                {
+                    form.displayInfo(u.ToString());
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ 19000562_GADE_Project/19000562_GADE_Project/Map.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19000562_GADE_Project
+{
+    class Map
+    {
+        public Unit[] units = new Unit[10];//new array with 10 elements
+
+        public Map()
+        {
+            Random r = new Random();//creating new random number
+            for(int i = 0; i < 10; i++)//looping through each element
+            {
+                int newX = r.Next(0, 20);//randomise x and y position 
+                int newY = r.Next(0, 20);
+                int team = i % 2;
+                int tempAttack = 0;
+                switch(r.Next(0,4))//getting random number betweeen 0 and 3 included
+                {
+                    case 0://if 0 then attack equal to 5
+                        tempAttack = 5;
+                        break;
+                    case 1:
+                        tempAttack = 10;
+                        break;
+                    case 2:
+                        tempAttack = 15;
+                        break;
+                    case 3:
+                        tempAttack = 20;
+                        break;     
+                }
+
+                switch (r.Next(0, 2))//getting random number between 0 and 1 included
+                {
+                    case 0://if its 0 then do meleeunit
+                        units[i] = new MeleeUnit(newX, newY, 100, 1, tempAttack, 1, team, i.ToString());//updates the unit based on its changing x and y cord
+                        break;
+                    case 1://if its 1 then do rangeunit
+                        units[i] = new RangeUnit(newX, newY, 100, 1, tempAttack, 4, team, i.ToString());
+                        break;
+                }
+            }
+        }
+    }
+}
